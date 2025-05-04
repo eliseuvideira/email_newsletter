a6e1218d728ac70932058651b6db6ec5c70e4bd8
Eliseu Videira <123996811+eliseuvideira1@users.noreply.github.com>
2025-05-04 11:41:40 -0300

    fix: listener bind bug (#2)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("http://127.0.0.1:8000")?;
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
     run(listener)?.await
 }
