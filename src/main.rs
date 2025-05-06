8116746c2e040b8f8aa0ba439216c654077aea63
Eliseu Videira <123996811+eliseuvideira1@users.noreply.github.com>
2025-05-06 12:28:18 -0300

    feat: implement telemetry (#5)

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ use std::net::TcpListener;
 
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
