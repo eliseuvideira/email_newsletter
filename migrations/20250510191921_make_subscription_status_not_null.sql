4124c2230ef642e590c664f6eabac531b82f0cca
Eliseu Videira <eliseu@tryfinch.com>
2025-05-15 09:21:28 -0300

    fix: migration bug

--- migrations/20250510191921_make_subscription_status_not_null.sql
+++ migrations/20250510191921_make_subscription_status_not_null.sql
@@ -2,7 +2,7 @@ START TRANSACTION;
 
 UPDATE subscriptions
    SET status = 'confirmed'
- WHERE status = null;
+ WHERE status IS NULL;
 
 ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
 
